deafb8a718944ea64f5e8a677bbaada590115564
Zakaria Yaris <info@zakariya.me>
2023-03-07 23:38:20 +0000

    add tasks to list + add task functionality testing
    Relates #5

--- app.js
+++ app.js
@@ -1,45 +1,74 @@
-// for inttial development purposes
-const mockData = [
-  {
-    id: 1,
-    description: 'Go to the gym',
-    status: false,
-  },
-  {
-    id: 2,
-    description: 'Buy groceries',
-    status: false,
-  },
-  {
-    id: 3,
-    description: 'Call Amanda',
-    status: false,
-  },
-];
+// Todo list tasks
+const tasks = [];
 
-// create tasks dynamically using template
-const todoContainer = document.querySelector('.todo-list');
+const todoSection = document.querySelector('.todo-section');
+const todoContainer = document.querySelector('.todos');
+const formContainer = document.querySelector('.forms');
 
-const createTasks = (tasks) => {
-  const { description } = tasks;
+// ----------------------------
+// display the tasks on the page
+const displayTasks = (tasks) => {
+  const { task } = tasks;
 
-  const template = document.querySelector('#template');
-  const domFragment = template.content.cloneNode(true);
+  const todoTemplate = document.querySelector('#todo-template');
+  const domFragment = todoTemplate.content.cloneNode(true);
 
-  // add task description into the element
-  domFragment.querySelector('.task').textContent = description;
+  // add task task into the element
+  domFragment.querySelector('.task').textContent = task;
 
   // add the fragment into the document
   todoContainer.appendChild(domFragment);
 };
 
-mockData.forEach(createTasks);
+// ----------------------------
+// create tasks dynamically and add them to array using template
+const addTask = (task) => {
+  tasks.push(task);
+};
 
-function addTask() {
-}
+const createTask = () => {
+  // clear the canvas first
+  formContainer.innerHTML = '';
 
-function markTaskAsComplete() {
-}
+  const formTemplate = document.querySelector('#form-template');
+  const domFragment = formTemplate.content.cloneNode(true);
 
-function deleteTask() {
-}
+  // display the template form.
+  formContainer.appendChild(domFragment);
+
+  const taskInput = document.querySelector('.task-input');
+  const submitTaskBtn = document.querySelector('.submit-task');
+
+  // create a task once user clicks submit
+  submitTaskBtn.addEventListener('click', async (e) => {
+    e.preventDefault();
+
+    // get a unique id to attach to each task
+    const response = await fetch('https://www.uuidgenerator.net/api/version1', {
+      headers: { Accept: 'application/json' },
+    });
+    const responseJson = await response.json();
+    const id = responseJson[0];
+
+    const taskValue = taskInput.value;
+    const done = false;
+
+    const task = { id, task: taskValue, done };
+
+    // add the task to the list
+    addTask(task);
+
+    // clear the previous tasks first before updating
+    todoContainer.innerHTML = '';
+    tasks.forEach(displayTasks);
+
+    // hide/delete the task form
+    document.querySelector('.task-form').style.display = 'none';
+  });
+};
+
+document.querySelector('.add-task').addEventListener('click', createTask);
+
+// const markTaskAsComplete = () => {};
+
+// const deleteTask = () => {};
