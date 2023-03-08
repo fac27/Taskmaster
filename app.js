8190df3bde8d3717208ed7df37d073bd65ed735a
Zakaria Yaris <info@zakariya.me>
2023-03-08 20:49:39 +0000

    add delete task functionality + its unit test
    Relates #6

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Todo list tasks
-const tasks = [];
+const tasks = [{ task: 'Go to the gym', done: false, id: 18 }];
 
 const todoSection = document.querySelector('.todo-section');
 const todoContainer = document.querySelector('.todos');
@@ -15,6 +15,15 @@ const displayTasks = (tasks) => {
 
   // add task task into the element
   domFragment.querySelector('.task').textContent = task;
+
+  // add event listener to delete the task
+  const deleteBtn = domFragment.querySelector('.delete-btn');
+  const taskText = getTaskText(deleteBtn);
+  deleteBtn.addEventListener('click', () => deleteTask(taskText));
+
+  // add event listener to complete the task
+  // const checkbox = domFragment.querySelector('#checkbox');
+  // checkbox.addEventListener('click', markTaskAsComplete);
 
   // add the fragment into the document
   todoContainer.appendChild(domFragment);
@@ -44,16 +53,16 @@ const createTask = () => {
     e.preventDefault();
 
     // get a unique id to attach to each task
-    const response = await fetch('https://www.uuidgenerator.net/api/version1', {
-      headers: { Accept: 'application/json' },
-    });
-    const responseJson = await response.json();
-    const id = responseJson[0];
+    // const response = await fetch('https://www.uuidgenerator.net/api/version1', {
+    //   headers: { Accept: 'application/json' },
+    // });
+    // const responseJson = await response.json();
+    // const id = responseJson[0];
 
     const taskValue = taskInput.value;
     const done = false;
 
-    const task = { id, task: taskValue, done };
+    const task = { task: taskValue, done };
 
     // add the task to the list
     addTask(task);
@@ -69,6 +78,28 @@ const createTask = () => {
 
 document.querySelector('.add-task').addEventListener('click', createTask);
 
-// const markTaskAsComplete = () => {};
+// ----------------------------
+// delete tasks from list and array
+const getTaskText = (e) => {
+  return e.parentElement.parentElement.children[1].textContent;
+};
 
-// const deleteTask = () => {};
+const deleteTask = (taskText) => {
+  const task = tasks.filter((task) => task.task === taskText)[0];
+  // remove the task from the array
+  tasks.splice(tasks.indexOf(task), 1);
+
+  // update the page
+  todoContainer.innerHTML = '';
+  tasks.forEach(displayTasks);
+};
+
+// ----------------------------
+// mark completed tasks
+const markTaskAsComplete = (taskText, status) => {
+  console.log(taskText);
+};
+
+// ----------------------------
+// edit tasks
+// const editTask = (task) => {};
