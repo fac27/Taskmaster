59496b3cb953bb5a43b79616bd587dd6faa0cda0
carl <carl.cross@yahoo.co.uk>
2023-03-12 08:23:19 +0000

    1st iteration if end to end tests
    Relates #21

--- index.test.js
+++ index.test.js
@@ -19,3 +19,107 @@
 //     .done;
 //   equal(actual, expected);
 // });
+
+// -----------------------------
+// END TO END TESTS
+
+function addTaskTest() {
+  // Find the add new task input element
+  const addNewTask = document.querySelector('.task-input');
+
+  // Set the values of the input elements
+  addNewTask.value = 'Mop the floor';
+
+  // Find the 'submit' button
+  const submitButton = document.querySelector('.add-task');
+
+  // Simulate a click event on the submit button
+  submitButton.click();
+
+  // Find the 'result' element
+  const resultElement = todoContainer.querySelector('.todo-list');
+
+  // Check if the text content of the 'result' element matches the expected value
+  if (resultElement.textContent.includes(addNewTask.value)) {
+    console.log('Test passed: Task added successfully');
+  } else {
+    console.error('Test failed.');
+  }
+
+}
+
+addTaskTest();
+
+function editTaskTest() {
+  // Find the add new task input element
+  const addNewTask = document.querySelector('.task-input');
+
+  // Set the values of the input elements
+  addNewTask.value = 'Mop the floor';
+
+  // Find the 'submit' button
+  const submitButton = document.querySelector('.add-task');
+
+  // Simulate a click event on the submit button
+  submitButton.click();
+
+  // Find the 'result' element
+  const resultElement = todoContainer.querySelector('.todo-list');
+
+  // Find the edit button of the task
+  const editButton = resultElement.querySelector('.edit-btn');
+
+  // Simulate a click event on the edit button
+  editButton.click();
+
+  // Change the value entered in add new task
+  addNewTask.value = 'Vacuum the floor';
+
+  // Simulate a click event on the submit button to save the changes
+  submitButton.click();
+
+  // Find the task element
+  const taskElement = resultElement.querySelector('.task');
+
+  // Check if the text content of the task element matches the expected value
+  if (taskElement.textContent === 'Vacuum the floor') {
+    console.log('Test passed: Task edited successfully!');
+  } else {
+    console.error(`Task edit failed. Expected "Vacuum the floor", but got "${taskElement.textContent}"`);
+  }
+}
+
+editTaskTest();
+
+function deleteTaskTest() {
+  // Find the add new task input element
+  const addNewTask = document.querySelector('.task-input');
+
+  // Set the values of the input elements
+  addNewTask.value = 'Mop the floor';
+
+  // Find the 'submit' button
+  const submitButton = document.querySelector('.add-task');
+
+  // Simulate a click event on the submit button
+  submitButton.click();
+
+  // Find the 'result' element
+  const resultElement = todoContainer.querySelector('.todo-list');
+
+  // Find the delete button of the task
+  const deleteButton = resultElement.querySelector('.delete-btn');
+
+  // Simulate a click event on the delete button
+  deleteButton.click();
+
+  // Check if the task element no longer exists in the DOM
+  const taskElement = resultElement.querySelector('.task');
+  if (taskElement === null) {
+    console.log('Task deleted successfully!');
+  } else {
+    console.error('Task delete failed. Task element still exists in the DOM.');
+  }
+}
+
+deleteTaskTest()
